57ba9cb17781c9ca5e149ceda8cd9bd139b6bb06
Mario Martinez <zenitram.oiram@gmail.com>
2016-05-15 12:40:58 +1000

    Add more tests

--- recipe_finder_test.rb
+++ recipe_finder_test.rb
@@ -26,4 +26,35 @@ class RecipeFinderTest < MiniTest::Test
     recipe_finder = RecipeFinder.new('my-fridge-4.csv', 'my-recipe.json')
     assert_equal 'Call for takeout', recipe_finder.perform
   end
+
+  # Test 5: No recipes
+  def test_5
+    recipe_finder = RecipeFinder.new('my-fridge-1.csv', 'my-recipe-5.json')
+    assert_equal 'Call for takeout', recipe_finder.perform
+  end
+
+  # Test 6: Mismatched units - cheese in fridge is 'block', not 'slice'
+  def test_6
+    recipe_finder = RecipeFinder.new('my-fridge-6.csv', 'my-recipe.json')
+    assert_equal 'Vegemite Sandwich', recipe_finder.perform
+  end
+
+  # Test 7: Cheese expires first, multiple recipes with cheese
+  #         Recipe listed first expected.
+  def test_7
+    recipe_finder = RecipeFinder.new('my-fridge-3.csv', 'my-recipe-7.json')
+    assert_equal 'Cheese Omlette', recipe_finder.perform
+  end
+
+  # Test 8: Not enough cheese for omlette, other recipes valid
+  def test_8
+    recipe_finder = RecipeFinder.new('my-fridge-3.csv', 'my-recipe-8.json')
+    assert_equal 'Toasted Cheese', recipe_finder.perform
+  end
+
+  # Test 9: Empty fridge
+  def test_9
+    recipe_finder = RecipeFinder.new('my-fridge-9.csv', 'my-recipe.json')
+    assert_equal 'Call for takeout', recipe_finder.perform
+  end
 end
